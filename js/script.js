832fd7bc4f8e35e1e28209ec1d9ceabaffa4fd5f
Karambitos <pase4nikdv@gmail.com>
2020-04-15 15:50:23 +0300

    add button in progress

--- js/script.js
+++ js/script.js
@@ -34,18 +34,18 @@ document.addEventListener("DOMContentLoaded", () => {
                 body: 'Aliquip cupidatat ex adipisicing veniam do tempor. Lorem nulla adipisicing et esse cupidatat qui deserunt in fugiat duis est qui. Est adipisicing ipsum qui cupidatat exercitation. Cupidatat aliqua deserunt id deserunt excepteur nostrud culpa eu voluptate excepteur. Cillum officia proident anim aliquip. Dolore veniam qui reprehenderit voluptate non id anim.\r\n',
                 title: 'Deserunt laborum id consectetur pariatur veniam occaecat occaecat tempor voluptate pariatur nulla reprehenderit ipsum.',
             },
-            // {
-            //     _id: '5d2ca9e2e03d40b3232496aa7',
-            //     completed: true,
-            //     body: 'Occaecat non ea quis occaecat ad culpa amet deserunt incididunt elit fugiat pariatur. Exercitation commodo culpa in veniam proident laboris in. Excepteur cupidatat eiusmod dolor consectetur exercitation nulla aliqua veniam fugiat irure mollit. Eu dolor dolor excepteur pariatur aute do do ut pariatur consequat reprehenderit deserunt.\r\n',
-            //     title: 'Eu ea incididunt sunt consectetur fugiat non.',
-            // },
-            // {
-            //     _id: '5d2ca9e29c8a94095564788e0',
-            //     completed: false,
-            //     body: 'Aliquip cupidatat ex adipisicing veniam do tempor. Lorem nulla adipisicing et esse cupidatat qui deserunt in fugiat duis est qui. Est adipisicing ipsum qui cupidatat exercitation. Cupidatat aliqua deserunt id deserunt excepteur nostrud culpa eu voluptate excepteur. Cillum officia proident anim aliquip. Dolore veniam qui reprehenderit voluptate non id anim.\r\n',
-            //     title: 'Deserunt laborum id consectetur pariatur veniam occaecat occaecat tempor voluptate pariatur nulla reprehenderit ipsum.',
-            // },
+            {
+                _id: '5d2ca9e2e03d40b3232496aa7',
+                completed: true,
+                body: 'Occaecat non ea quis occaecat ad culpa amet deserunt incididunt elit fugiat pariatur. Exercitation commodo culpa in veniam proident laboris in. Excepteur cupidatat eiusmod dolor consectetur exercitation nulla aliqua veniam fugiat irure mollit. Eu dolor dolor excepteur pariatur aute do do ut pariatur consequat reprehenderit deserunt.\r\n',
+                title: 'Eu ea incididunt sunt consectetur fugiat non.',
+            },
+            {
+                _id: '5d2ca9e29c8a94095564788e0',
+                completed: false,
+                body: 'Aliquip cupidatat ex adipisicing veniam do tempor. Lorem nulla adipisicing et esse cupidatat qui deserunt in fugiat duis est qui. Est adipisicing ipsum qui cupidatat exercitation. Cupidatat aliqua deserunt id deserunt excepteur nostrud culpa eu voluptate excepteur. Cillum officia proident anim aliquip. Dolore veniam qui reprehenderit voluptate non id anim.\r\n',
+                title: 'Deserunt laborum id consectetur pariatur veniam occaecat occaecat tempor voluptate pariatur nulla reprehenderit ipsum.',
+            },
         ];
 
         //       **********************************************************************
@@ -66,28 +66,14 @@ document.addEventListener("DOMContentLoaded", () => {
             renderAllTasks(objOfTasks);
             form.addEventListener('submit', onFormSubmit);
             listConteiner.addEventListener('click', onClickDelete)
+            listConteiner.addEventListener('click', onClickProgress)
 
-            function emptyTaskList(taskList) {
-                const div = document.createElement('div');
-                div.classList.add('list-group-item', 'd-flex', 'align-items-center', 'flex-wrap', 'mt-2');
-                const span = document.createElement('span');
-                span.style.fontWeight = "bold";
-                span.style.margin = "auto";
-                span.textContent = "Список задач пуст";
-                div.appendChild(span)
 
-                if (Object.values(taskList).length == 0) {
-                    console.log("Список задач пуст")
-                    listConteiner.appendChild(div);
-                } else {
-                    console.log("Список задач HE пуст", Object.values(taskList).length)
-                }
-
-            }
 
             function renderAllTasks(taskList) {
-                if (!taskList) {
+                if (!taskList || Object.values(taskList).length == 0) {
                     console.error("NO tasks Add")
+                    emptyTaskListChek(objOfTasks);
                     return;
                 }
                 const fragment = document.createDocumentFragment();
@@ -95,14 +81,14 @@ document.addEventListener("DOMContentLoaded", () => {
                     const li = listItemTemplate(task);
                     fragment.appendChild(li);
                 })
-                // console.log(fragment)
                 listConteiner.appendChild(fragment)
             }
 
             function listItemTemplate({ // create <li></li>
                 _id,
                 title,
-                body
+                body,
+                completed,
             } = {}) {
                 const li = document.createElement('li');
                 li.classList.add('list-group-item', 'd-flex', 'align-items-center', 'flex-wrap', 'mt-2');
@@ -116,12 +102,28 @@ document.addEventListener("DOMContentLoaded", () => {
                 btnDelete.classList.add('lbtn', 'btn-danger', 'ml-auto', 'delete-btn');
                 btnDelete.textContent = "Delete";
                 // console.log(btn);
+
+                const btnDone = document.createElement('button');
+                btnDone.classList.add('lbtn', 'mr-auto', 'progress-btn');
+                btnDone.dataset.completed = completed;
+                const completedData = btnDone.dataset.completed;
+
+                if (completedData === "true") {
+                    btnDone.classList.add('btn-primary');
+                    btnDone.textContent = "Done";
+                } else {
+                    btnDone.classList.add('btn-secondary');
+                    btnDone.textContent = "in progress";
+                }
+
+                // console.log(btnDone);
                 const article = document.createElement('p');
                 article.classList.add('mt-2', 'w-100');
                 article.textContent = body;
                 // console.log(art);
                 li.appendChild(span);
                 li.appendChild(article);
+                li.appendChild(btnDone);
                 li.appendChild(btnDelete);
                 // console.log(li)
                 return li;
@@ -139,7 +141,6 @@ document.addEventListener("DOMContentLoaded", () => {
                 const task = createNewTask(titleValue, bodyValue);
                 const newLiItem = listItemTemplate(task);
                 listConteiner.insertAdjacentElement("afterbegin", newLiItem);
-                div.remove();
             }
 
             function createNewTask(title, body) { //add obj in obj
@@ -151,6 +152,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 };
                 objOfTasks[newTask._id] = newTask;
                 form.reset();
+                deleteDiv();
                 return newTask;
             }
 
@@ -169,7 +171,7 @@ document.addEventListener("DOMContentLoaded", () => {
             function deleteTaskHTML(confirmed, el) { //delete element
                 if (!confirmed) return;
                 el.remove();
-                emptyTaskList(objOfTasks);
+                emptyTaskListChek(objOfTasks);
             }
 
             function deleteTask(id) { //delete obj by id
@@ -178,6 +180,53 @@ document.addEventListener("DOMContentLoaded", () => {
                 if (!isConfirm) return isConfirm;
                 delete objOfTasks[id];
                 return isConfirm;
+            }
+
+            function createDiv() { // create <div>"Task list is empty"</div>
+                const div = document.createElement('div');
+                div.classList.add('list-group-item', 'd-flex', 'align-items-center', 'flex-wrap', 'mt-2');
+                div.id = "empty";
+                div.style.backgroundColor = "antiquewhite";
+                const span = document.createElement('span');
+                span.style.fontWeight = "bold";
+                span.style.margin = "auto";
+                span.textContent = "Task list is empty";
+                div.appendChild(span)
+                listConteiner.appendChild(div);
+            }
+
+            function deleteDiv() {
+                const element = document.getElementById('empty');
+                if (element) {
+                    element.remove();
+                }
+                return;
+            }
+
+            function emptyTaskListChek(taskList) {
+                if (Object.values(taskList).length == 0) {
+                    createDiv();
+                } else {
+                    console.log("Task list is NOT empty", Object.values(taskList).length);
+                }
+            }
+
+            function onClickProgress(e) {
+                const target = e.target;
+                const targetLi = target.closest('[data-task-id]');
+                const id = targetLi.dataset.taskId;
+                if (target.classList.contains('progress-btn' && 'btn-primary')) {
+                    target.classList.remove('btn-primary');
+                    target.classList.add('btn-secondary');
+                    target.textContent = "in progress";
+                    target.dataset.completed = `${objOfTasks[id].completed}`;
+                } else if (target.classList.contains('progress-btn' && 'btn-secondary')) {
+                    target.classList.remove('btn-secondary');
+                    target.classList.add('btn-primary');
+                    target.textContent = "Done";
+                    objOfTasks[id].completed = true;
+                    target.dataset.completed = `${objOfTasks[id].completed}`;
+                }
             }
         })(tasks);
     }
